a4f4f88f7248bce25eed3adc4892be2d4e0e12c8
Dmitrii Shusharin <mindraining@gmail.com>
2020-03-22 11:11:16 +0300

    upd1

--- src/main/java/actor/ChildActor.java
+++ src/main/java/actor/ChildActor.java
@@ -7,8 +7,7 @@ public class ChildActor extends UntypedActor {
     @Override
     public void onReceive(Object o) throws Throwable {
         if (o instanceof Request) {
-            Response response =
-                    new Response(((Request) o).getSearcher(), StubServer.getResponse((Request) o));
+            Response response = new Response(((Request) o).getSearcher(), StubServer.getResponse((Request) o));
             getSender().tell(response, getSelf());
             return;
         }
